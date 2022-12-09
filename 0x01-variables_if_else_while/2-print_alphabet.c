5458db6f2469b79375990dc10014cc28736130cf
Dave1-french <davidireti2001@gmail.com>
2022-12-09 09:07:57 -0800

    add task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char cat;
@@ -17,6 +15,5 @@ int main(void)
 		putchar(cat);
 		putchar('\n');
 	}
-
 	return (0);
 }
