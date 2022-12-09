14bb9a363337c4c73d54197d0e965551442cda3c
Dave1-french <davidireti2001@gmail.com>
2022-12-09 09:28:56 -0800

    added task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char cat;
+
+	for (cat = 'a'; cat <= 'z'; cat++)
+	{
+		putchar(cat);
+	}
+	for (cat='A'; cat <= 'Z'; cat++)
+	{
+		putchar(cat);
+	}
+		putchar('\n');
+	return (0);
+}
