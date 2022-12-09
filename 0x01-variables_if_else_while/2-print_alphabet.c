db4fb1ec2fb4c0aa890433b97919cb1a17e73c0d
Dave1-french <davidireti2001@gmail.com>
2022-12-09 09:01:45 -0800

    add task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char cat;
+
+	for (cat = 'a'; cat <= 'z'; cat++)
+	{
+		putchar(cat);
+		putchar('\n');
+	}
+
+	return (0);
+}
