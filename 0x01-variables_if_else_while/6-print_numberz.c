11548350ab563bf1db1a6692f50c833c18090876
Dave1-french <davidireti2001@gmail.com>
2022-12-09 14:27:07 -0800

    added task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+	return (0);
+}
