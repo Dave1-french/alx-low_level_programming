426e100494a5880e68d304f9df5e4e4b349b3969
Dave1-french <davidireti2001@gmail.com>
2022-12-13 09:09:47 -0800

    added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(c[i]);
+
+	_putchar('\n');
+	return (0);
+}
