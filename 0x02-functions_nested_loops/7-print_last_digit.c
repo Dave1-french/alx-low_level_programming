6119421bcb9b11985cfbcd62647f8bac9e394081
Dave1-french <davidireti2001@gmail.com>
2022-12-13 14:49:59 -0800

    added task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,10 @@ int print_last_digit(int n)
 {
 	int lastdigit = n % 10;
 
-	if (lastdigit >= 0)
-		_putchar(lastdigit + '0');
-	else
-	{
+	if (lastdigit < 0)
 		lastdigit *= -1;
-		_putchar(lastdigit + '0');
-	}
+
+	_putchar(lastdigit + '0');
+
 	return (last_digit);
 }
