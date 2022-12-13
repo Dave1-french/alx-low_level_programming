9d94d546a086c55cdc063e9b8ecc7cb8614a11b4
Dave1-french <davidireti2001@gmail.com>
2022-12-13 14:01:27 -0800

    added task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: 'function that computes the absolute value of an integer'
+ * @n: integer to be computed
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+
+}
