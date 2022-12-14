7264569ae0e1910f038765a513cbad84d491d3d0
Dave1-french <davidireti2001@gmail.com>
2022-12-14 08:08:48 -0800

    added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * add - Entry point
+ *
+ * Description: 'function that adds two integers and returns the result'
+ *@cat1: first number add
+ *@cat2: twice number add
+ * Return: result of addition
+ */
+
+int add(int cat1, int cat2)
+{
+	return (cat1 + cat2);
+}
