9e515bfc8d30a858789f84a6bc318d719e3d2ff1
David MAMADOU <david@kali>
2022-12-15 22:20:09 +0100

    added task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: 'function that multiplies two integers'
+ * @a: first integer 
+ * @b: second integer
+ * Return: result of multiplies
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
