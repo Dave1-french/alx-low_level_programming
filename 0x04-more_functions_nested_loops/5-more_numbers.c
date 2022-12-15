2e9efa3d48701fe8881ab2b67a88f1fd6a84a906
David MAMADOU <david@kali>
2022-12-16 00:02:10 +0100

    added task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times numbers from 0 to 14\n
+ *
+ * Return: void
+ */
+
+
+void more_numbers(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = '0'; c <= '14'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
