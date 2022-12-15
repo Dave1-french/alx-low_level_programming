dd800b7b175cf9d2b4f67266f7b57ecbcecd379e
David MAMADOU <david@kali>
2022-12-15 21:44:00 +0100

    added task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * Description: 'function that checks for a digit (0 through 9)
+ * @c: checked digit
+ * Return: 1 if c is a digit or 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <=9)
+		return (1);
+	else
+		return (0);
+}
