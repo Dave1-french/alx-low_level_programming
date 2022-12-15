fff6997a24efa043989ba7cef7a724a3621071e0
David MAMADOU <david@kali>
2022-12-16 00:52:18 +0100

    added task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,15 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n > 0)
 	{
-		_putchar('_');
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	if (n <= 0)
+	else
 	{
 		_putchar('\n');
 	}
