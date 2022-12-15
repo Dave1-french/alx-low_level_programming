b299ea17ee1f8e8469d6557c7f28c29f07c252bc
David MAMADOU <david@kali>
2022-12-15 17:53:58 +0100

    added task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-	int i;/*declaration of i*/
+	int i;
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;/*initialize of i*/
-
-	while (i < 10)/*while i is less than 10 print i*/
+	i = 0;
+/*
+	while (i < 10)
 	{
 		putchar(i);
 	}
-
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
