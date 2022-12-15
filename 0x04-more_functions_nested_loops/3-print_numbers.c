57682e61195e797b5154bfcc100780d7be93f44c
David MAMADOU <david@kali>
2022-12-15 22:41:41 +0100

    added task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * Description: 'function that print the number,from 0 to 9'
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+		_putchar('\n');
+	}
+}
