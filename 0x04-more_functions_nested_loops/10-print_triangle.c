d390c267491336ac2c1ddf4565ae97355ec6c356
David MAMADOU <david@kali>
2022-12-16 16:42:05 +0100

    added task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,9 @@ void print_triangle(int size)
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 1; j <= size; j++)
 			{
 				if (j <= (size - i))
 					_putchar(' ');
