c6a185d30e52e49ff7075e6d21d889684fe74986
David MAMADOU <david@kali>
2022-12-16 03:04:25 +0100

    added task 8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints numbers 1 t0 100\n, 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3)
+			printf("Fizz");
+		else if (i % 5)
+			printf("Buzz");
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
