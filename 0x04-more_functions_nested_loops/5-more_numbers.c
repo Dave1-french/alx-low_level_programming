6240d1646bc0ce22b91e35ded1459ec4810ab279
David MAMADOU <david@kali>
2022-12-16 03:22:16 +0100

    added task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,19 @@
  * Return: void
  */
 
-
 void more_numbers(void)
 {
-	int i, c;
 
-	for (i = 0; i < 10; i++)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-		for (c = 0; c < 15; c++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
 			_putchar('1');
-			_putchar(c % 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 			_putchar('\n');
-	}
+		}
 }
+
