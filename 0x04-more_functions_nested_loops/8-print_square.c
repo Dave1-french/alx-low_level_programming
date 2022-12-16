0728d1896d4786177f2f0eb2ddcae733d827c199
David MAMADOU <david@kali>
2022-12-16 02:36:30 +0100

    added task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square\n
+ * @size: checked
+ */
+
+void print_square(int size);
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
