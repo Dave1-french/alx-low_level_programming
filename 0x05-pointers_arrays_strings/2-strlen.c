bd881b4775546e3490cc52653065611a80fd7917
David MAMADOU <david@kali>
2022-12-20 22:05:44 +0100

    added task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen - function the length of a string
+ *@s: string checked
+ *Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif
