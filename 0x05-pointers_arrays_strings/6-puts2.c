109406ab48f30deee6623245b0d6d211f894d659
David MAMADOU <david@kali>
2022-12-21 01:22:08 +0100

    added  task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2- prints every other character of a string\n
+ *@str: string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
+
