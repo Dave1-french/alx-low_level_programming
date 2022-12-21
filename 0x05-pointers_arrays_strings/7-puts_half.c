2597e219d587c30d76f8d5ee28d7a75238b9096f
David MAMADOU <david@kali>
2022-12-21 01:45:47 +0100

    added task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *puts_half- prints half of a string\n
+ *@str: string
+ */
+
+void puts_half(char *str)
+{
+	int i, n, len;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	n = (len / 2);
+
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
