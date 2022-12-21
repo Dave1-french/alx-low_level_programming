a0a9e8ae1b6e254cc26567bffd61e65c39ae6216
David MAMADOU <david@kali>
2022-12-21 02:18:34 +0100

    added task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,6 +7,7 @@
  * @n:  number of elements of the array
  * Return: a and n inputs
  */
+
 void print_array(int *a, int n)
 {
 	int i;
@@ -14,7 +15,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < (n - 1); i++)
 		printf("%d, ", a[i]);
 		if (i == (n - 1))
-			printf("%d%", a[n - 1]);
+			printf("%d", a[n - 1]);
 	printf("\n");
 }
 
