6f439e233196c3fc5ca8f594008e11b034e51133
David MAMADOU <david@kali>
2023-01-02 17:12:22 +0100

    added task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ *@s: pointer destination
+ *@b: constant byte
+ *@n: number of byte
+ *Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
