fed745857dfbd6c2e039f502e7f7aa17c4b6b691
David MAMADOU <david@kali>
2023-01-06 22:10:19 +0100

    added task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,11 +6,13 @@
  * @argv: array pointing to arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	int i;
 
-	(void) argv;
-	printf("%d\n", argc - 1);
+	for (i = 0; i < argc; i++)
+	{
+	}
+	printf("%d\n", i - 1);
 	return (0);
 }
