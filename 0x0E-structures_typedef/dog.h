0b1f86780745b7bc84fc2109c15a6111fd6b5348
David MAMADOU <david@kali>
2023-01-16 22:16:05 +0100

    added task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,25 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
 
 /**
- * struct dog - dog presentation
- * @name: First element
- * @age: Second element
- * @owner: third element
- *
- * Description: it's presentation of dog
+ * struct dog - dog structure.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
  */
 
 struct dog
 {
 	char *name;
-	char age;
+	float age;
 	char *owner;
 };
 
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+void free_dog(dog_t *d);
 
 #endif
